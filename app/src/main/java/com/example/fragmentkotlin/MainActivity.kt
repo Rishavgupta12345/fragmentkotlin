237645a89d9e942d95f2db05d5788b5074cab8ef
rishavgupta <rishavgupta933@gmail.com>
2021-08-24 18:25:23 +0530

    fragment interaction

--- app/src/main/java/com/example/fragmentkotlin/MainActivity.kt
+++ app/src/main/java/com/example/fragmentkotlin/MainActivity.kt
@@ -3,46 +3,37 @@ package com.example.fragmentkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Onefragment.OnMessageClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addFragment = findViewById<Button>(R.id.add_fragment)
-        addFragment.setOnClickListener {
-            val fragment = Onefragment()
+        val fragment = Onefragment()
 
-            val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.add(R.id.main_container, fragment, "OneFragment")
-            fragmentTransaction.commit()
-        }
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.main_container, fragment, "OneFragment")
+        fragmentTransaction.commit()
 
-        val removeFragment = findViewById<Button>(R.id.remove_fragment)
-        removeFragment.setOnClickListener {
-            val fragment = supportFragmentManager.findFragmentById(R.id.main_container)
-            //val fragmentOne = supportFragmentManager.findFragmentByTag("OneFragment")
-
-            fragment?.let{
-                supportFragmentManager.beginTransaction().remove(fragment).commit()
-            }
-        }
-
-
-        val replaceFragment = findViewById<Button>(R.id.replace_fragment)
-        replaceFragment.setOnClickListener {
-            val fragment = Twofragment()
-
-            with(supportFragmentManager.beginTransaction()){
-                replace(R.id.main_container, fragment)
-                addToBackStack(null)
-                setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                commit()
-            }
+        val showFragmentMessage = findViewById<Button>(R.id.show_fragment_message)
+        showFragmentMessage.setOnClickListener {
+            val fragment = supportFragmentManager.findFragmentById(R.id.main_container) as Onefragment
+            fragment.showFragmentMessage()
         }
     }
+
+
+    override fun onMessageClick() {
+        showActivityMessage()
+    }
+
+    fun showActivityMessage(){
+        Toast.makeText(this, "This message is from Activity", Toast.LENGTH_SHORT).show()
+    }
 }
+
 
 
 
